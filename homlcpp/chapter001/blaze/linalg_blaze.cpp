a764749c3137fdc111f8d02327bb9e80457d1d50
fahmi ahmad <fahmiahmad@tutanota.com>
2025-05-16 20:52:55 +0700

    blaze init

---
+++ homlcpp/chapter001/blaze/linalg_blaze.cpp
@@ -0,0 +1,16 @@
+#include <blaze/Math.h>
+#include <iostream>
+#include "blaze/math/TransposeFlag.h"
+#include "blaze/math/dense/UniformVector.h"
+#include "blaze/math/expressions/Forward.h"
+#include "blaze/math/sparse/IdentityMatrix.h"
+#include "blaze/util/Random.h"
+
+typedef blaze::StaticMatrix<float, 3UL, 3UL, blaze::columnMajor> MyMatrix33f;
+typedef blaze::StaticVector<float, 3UL> MyVector3f;
+typedef blaze::DynamicMatrix<double> MyMatrix;
+
+int main()
+{
+
+}
